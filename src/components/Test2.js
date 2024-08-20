ca641dbced8b9200b80a40b4c40aeb4ee7030255
s5gouri <sahilgouri2005@gmail.com>
2024-08-21 01:03:03 +0530

    f

--- src/components/Test2.js
+++ src/components/Test2.js
@@ -1,16 +1,7 @@
 import React from 'react'
-// import { GoogleLogout } from "react-google-login";
 
-const client_id =
-  "216704398302-adt18t71erlvf07dl13okgeeotqfgeuo.apps.googleusercontent.com";
 export const Test2 = () => {
   return (
-    <div>
-        <GoogleLogout
-            clientId={client_id}
-            buttonText="Logout"
-            onLogoutSuccess={() => alert("You have been logged out successfully")}
-        />
-    </div>
+    <div>Test2</div>
   )
 }
--- src/components/Testing.js
+++ src/components/Testing.js
@@ -1,24 +1,7 @@
-import React from "react";
-// import { GoogleLogin } from "react-google-login";
-const client_id =
-  "216704398302-adt18t71erlvf07dl13okgeeotqfgeuo.apps.googleusercontent.com";
+import React from 'react'
+
 export const Testing = () => {
-  const onSuccess = (res) => {
-    console.log("Login Success: currentUser:", res.profileObj);
-  };
-  const onFailure = (res) => {
-    console.log("Login Failed: res:", res);
-  };
   return (
-    <div id="signInButton">
-      <GoogleLogin
-        clientId={client_id}
-        buttonText="Login"
-        onSuccess={onSuccess}
-        onFailure={onFailure}
-        cookiePolicy={"single_host_origin"}
-        isSignedIn={true}
-      />
-    </div>
-  );
-};
+    <div>Testing</div>
+  )
+}

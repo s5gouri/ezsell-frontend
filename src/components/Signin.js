b0da2f2b1f8ebf02eb96ce64ded6a1186e76884e
s5gouri <sahilgouri2005@gmail.com>
2024-08-22 04:00:09 +0530

    f

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -61,7 +61,7 @@ export const Signin = () => {
   };
   const loginwithgoogle = () => {
     window.open(
-      "http://192.168.5.161:8000/auth/google?signup=noo",
+      "https://ezsell-backend.vercel.app/auth/google?signup=noo",
       "_self"
     );
   };
